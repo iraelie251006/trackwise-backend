37702938ff866debbee89e2a9be0fc8e7423c3f9
Elie <niyubwayoiraelie5777@gmail.com>
2025-07-04 23:39:58 +0200

    Update package-lock.json and package.json to include new dependencies

---
+++ src/config/env.ts
@@ -0,0 +1,13 @@
+import { config } from "dotenv";
+import { envSchema } from "../utils/validations";
+
+config({path: '.env'});
+
+const _env = envSchema.safeParse(process.env);
+
+if (!_env.success) {
+    console.error("Invalid environment variables:", _env.error.format());
+    throw new Error("Invalid environment variables");
+}
+
+export const {PORT} = _env.data;
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// src/index.ts
+import express from 'express';
+import { PORT } from './config/env';
+
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.json({ message: 'Welcome to TrackWise API' });
+});
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
