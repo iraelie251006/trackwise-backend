0fdb144bd50642acd535167502861e642a82635f
Elie <niyubwayoiraelie5777@gmail.com>
2025-07-18 23:59:47 +0200

    Initialized controllers

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { SignIn, SignUp } from "../controllers/auth.controller";
 
 const authRouter = Router();
 
-authRouter.post("/sign-in", () => {});
+authRouter.post("/sign-in", SignIn);
 
-authRouter.post("/sign-up", () => {});
+authRouter.post("/sign-up", SignUp);
 
 export default authRouter;
