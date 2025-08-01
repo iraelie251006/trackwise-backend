8e5993f27903266217e7773f5fc1b6b0a1dcfe62
Elie <niyubwayoiraelie5777@gmail.com>
2025-08-01 23:57:06 +0200

    Implemented authorization logic

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,3 +1,31 @@
 import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { ACCESS_TOKEN_SECRET } from "../config/env";
+import { prisma } from "../utils/prisma";
 
-export const authorize = async (req: Request, res: Response, next: NextFunction) => {}
+export const authorize = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        let token;
+
+        if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+            token = req.headers.authorization.split(" ")[1];
+        };
+
+        const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
+
+        const user = await prisma.user.findUnique(decoded.userId);
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                error: { message: "Unauthorized user." },
+            });
+        };
+
+        req.user = user;
+        
+        next();
+    } catch (error) {
+        next(error)
+    }
+}
