ff47e2ef02256d82cdea33ec5b0a3e5a6587befe
Elie <niyubwayoiraelie5777@gmail.com>
2025-08-01 23:23:16 +0200

    Added notFound middleware

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { PORT } from "./config/env";
 import authRouter from "./routes/auth.routes";
+import { notFound } from "./middlewares/notFound";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(notFound)
 
 app.use("/api/auth", authRouter);
 
